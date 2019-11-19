62eb87eee4513924a35ec63dc9ab088df48780a4
LeoRoma <xiajtn@gmail.com>
2019-11-19 19:32:24 +0000

    rendered title of the api

--- src/app.js
+++ src/app.js
@@ -7,8 +7,10 @@ $(function () {
     type: 'GET',
     url: 'https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=mEFtmwXR1fltqq8rPx8PKwTLKmktbqhO',
     success: function (results) {
-      $.each(results, function (i, result) {
-        $results.append('<li>result: ' + result);
+      var items = results.results;
+      console.log(items);
+      $.each(items, function (i, item) {
+        $results.append('<li>result: ' + item.title);
       });
     }
   });
