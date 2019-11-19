544da8274a331121ea0ebfec496a90b49cf24c55
LeoRoma <xiajtn@gmail.com>
2019-11-19 21:52:23 +0000

    iterate through results array and retrieved first three key of each hashes

--- src/app.js
+++ src/app.js
@@ -8,9 +8,10 @@ $(function () {
     url: 'https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=mEFtmwXR1fltqq8rPx8PKwTLKmktbqhO',
     success: function (results) {
       var items = results.results;
-      console.log(items);
+      
       $.each(items, function (i, item) {
-        $results.append('<li>result: ' + item.title);
+        $results.append('<li> Section: ' + item.section + ', Subsection: '+ item.subsection + ',Title: ' + item.title + '</li>');
+        console.log(item);
       });
     }
   });
