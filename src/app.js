704c28bf8043525c47ac5726118b8f76747c89b1
LeoRoma <xiajtn@gmail.com>
2021-02-09 15:23:53 +0000

    Changed fetch method

--- src/app.js
+++ src/app.js
@@ -1,21 +1,20 @@
-$(function () {
+let items = [];
 
-  var $results = $('#results');
-
-
-  $.ajax({
-    type: 'GET',
-    url: 'https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=mEFtmwXR1fltqq8rPx8PKwTLKmktbqhO',
-    success: function (results) {
-      var items = results.results;
-      
-      $.each(items, function (i, item) {
-        $results.append('<li> Section: ' + item.section + ', Subsection: '+ item.subsection + ',Title: ' + item.title + '</li>');
-        console.log(item);
-      });
+fetch('https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=s3sidQNE043xhMtYMOTXG0E044n5RsFd') // Change different Api to test
+  .then(
+    function(response){
+      if(response.status !== 200){
+        console.log(`Looks like there was some problem. Status Code: ${response.status}`);
+        return;
+      }
+      response.json()
+        .then(function(data) {
+          items.push(data.results);
+        })
     }
-  });
+  )
+  .catch(function(error){
+    console.log(`Fetch Error :-S ${error}`);
+  })
 
-
-});
-
+  console.log(items);
